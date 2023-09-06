dbe318ffb17869c95af1b55ffe6886cebbc412ec
jessicacorrea <jessicamilenacorrea@gmail.com>
2023-09-06 19:44:53 -0300

    Ajustes de visualização e consumo de dados com viewmodel

--- app/src/main/java/com/example/comics/view/MainActivity.kt
+++ app/src/main/java/com/example/comics/view/MainActivity.kt
@@ -6,13 +6,14 @@ import android.view.View
 import androidx.lifecycle.coroutineScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.comics.databinding.ActivityMainBinding
-import com.example.comics.interactor.Interactor
-import com.example.comics.presenter.Presenter
+import com.example.comics.model.ResultModel
+import com.example.comics.util.Result
+import com.example.comics.viewmodel.ComicsViewModel
 import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 
-class MainActivity : AppCompatActivity(), IView {
-
-    private val interactor: Interactor = Interactor(Presenter(this))
+class MainActivity : AppCompatActivity() {
+    private val viewModel: ComicsViewModel by inject()
 
     private var binding: ActivityMainBinding? = null
 
@@ -21,27 +22,34 @@ class MainActivity : AppCompatActivity(), IView {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        refrash()
+        viewModel.result.observe(this){
+            when (it) {
+                is Result.Success -> viewList(it.data.data.results)
+                is Result.Failure -> showError()
+            }
+        }
 
+        refresh()
         swipeList()
     }
 
+
     private fun swipeList() = with(binding?.swipeRefresh) {
         this?.setOnRefreshListener {
-            refrash()
+            refresh()
         }
     }
 
-    override fun refrash() {
+     private fun refresh() {
         with(binding) {
             this?.swipeRefresh?.isRefreshing = true
             lifecycle.coroutineScope.launch {
-                interactor.getComics()
+                viewModel.getListComics()
             }
         }
     }
 
-    override fun viewList(list: List<ItemVO>) {
+     private fun viewList(list: List<ResultModel>) {
         with(binding) {
             this?.errorTV?.visibility = View.GONE
             this?.listItem?.visibility = View.VISIBLE
@@ -51,12 +59,11 @@ class MainActivity : AppCompatActivity(), IView {
         }
     }
 
-    override fun error() {
+     private fun showError() {
         with(binding) {
             this?.listItem?.visibility = View.GONE
             this?.errorTV?.visibility = View.VISIBLE
             this?.swipeRefresh?.isRefreshing = false
         }
     }
-
 }
